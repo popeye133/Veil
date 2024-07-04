c17441de9a28bfecd8c8c215a808a630fc1e4a57
popeye <116702408+popeye133@users.noreply.github.com>
2024-07-04 10:38:00 +0700

    Refactor Conductor class for Veil framework with enhanced security, modularity, and error handling
    check out !! the code !!

--- lib/common/orchestra.py
+++ lib/common/orchestra.py
@@ -1,30 +1,29 @@
 """
-This is the conductor which controls everything
+This class controls the Veil framework and its operations.
 """
 
-import glob
 import os
-import readline
 import sys
+import readline
+import glob
+import subprocess
+import platform
+
 from lib.common import completer
 from lib.common import helpers
 from lib.common import messages
 
-
-# Try to find and import the settings.py config file
+# Attempt to import settings.py from /etc/veil/
 try:
     sys.path.append("/etc/veil/")
     import settings
 except ImportError:
-    print( "\n [!] ERROR #1-2: Can't import /etc/veil/settings.py.   Run: %s\n" % ( os.path.abspath( "./config/update-config.py" ) ) )
-    sys.exit()
+    print("\n [!] ERROR: Unable to import /etc/veil/settings.py. Please run: {}\n".format(os.path.abspath("./config/update-config.py")))
+    sys.exit(1)
 
 
 class Conductor:
-
     def __init__(self, cli_stuff):
-        # Create dictionaries of supported modules
-        # empty until stuff loaded into them
         self.imported_tools = {}
         self.load_tools(cli_stuff)
         self.mainmenu_commands = {
@@ -33,58 +32,43 @@ class Conductor:
             "info": "Information on a specific tool",
             "options": "Show Veil configuration",
             "update": "Update Veil",
-            "exit": "Completely exit Veil"}
-        self.number_of_tools = len(self.imported_tools)
+            "exit": "Completely exit Veil"
+        }
         self.command_line_options = cli_stuff
 
     def command_line_use(self):
         tool_found = False
-        for key, tool_object in sorted(self.imported_tools.items()):
-            # If the entered number matches the payload, use that payload
+        for tool_object in self.imported_tools.values():
             if self.command_line_options.tool.lower() == tool_object.cli_name.lower():
                 tool_object.cli_menu()
                 tool_found = True
+                break
         if not tool_found:
-            print(helpers.color(' [!] ERROR: You did not provide a valid tool name!', warning=True))
-            sys.exit()
+            print(helpers.color(' [!] ERROR: Invalid tool name provided!', warning=True))
+            sys.exit(1)
 
-    def list_tools(self, show_header = True):
-        # Did we run a command?
+    def list_tools(self, show_header=True):
         if show_header:
-            # show title bar
             messages.title_screen()
             print(helpers.color(' [*] Available Tools:\n'))
         else:
             print("Available Tools:\n")
 
-        # Loop over all tools loaded into Veil, print name and description
-        # Function for listing all payloads
-        tool_counter = 1
-        for key, tool in sorted(self.imported_tools.items()):
-            print('\t' + str(tool_counter) + ")\t" + tool.cli_name)
-            tool_counter += 1
+        for index, tool in enumerate(sorted(self.imported_tools.values(), key=lambda x: x.cli_name), start=1):
+            print('\t{})\t{}'.format(index, tool.cli_name))
         print()
-        return
 
     def load_tools(self, command_line_object):
-        # Load all tools within the Tools folder
-        # (Evasion, Ordnance, Pillage, etc.)
-        for name in glob.glob('tools/*/tool.py'):
-            if name.endswith(".py") and ("__init__" not in name):
-                module = helpers.load_module(name)
-                self.imported_tools[name] = module.Tools(
-                    command_line_object)
-        return
+        for tool_file in glob.glob('tools/*/tool.py'):
+            if tool_file.endswith(".py") and "__init__" not in tool_file:
+                module = helpers.load_module(tool_file)
+                if module:
+                    self.imported_tools[tool_file] = module.Tools(command_line_object)
 
     def main_menu(self):
-        # default blank command for the main menu loop
-        main_menu_command = ""
         show_header = True
 
-        # Try except to catch keyboard interrupts
         try:
-
-            # Loop for the main menu, will always loop as long as command is ''
             while True:
                 comp = completer.VeilMainMenuCompleter(self.mainmenu_commands, self.imported_tools)
                 readline.set_completer_delims(' \t\n;')
@@ -94,99 +78,64 @@ class Conductor:
                 if show_header:
                     messages.title_screen()
                     print("Main Menu")
-                    print("\n\t" + helpers.color(len(self.imported_tools)) + " tools loaded\n")
-                    # List tools, but don't show the header
+                    print("\n\t{} tools loaded\n".format(len(self.imported_tools)))
                     self.list_tools(False)
                     print("Available Commands:\n")
-                    for command in sorted(self.mainmenu_commands.keys()):
-                        print("\t" + helpers.color(command) + '\t\t\t' + self.mainmenu_commands[command])
+                    for command, description in sorted(self.mainmenu_commands.items()):
+                        print("\t{}\t\t{}".format(helpers.color(command), description))
                     print()
                     show_header = False
 
                 main_menu_command = input('Veil>: ').strip()
 
                 if main_menu_command.startswith('use'):
-                    # Check to make sure a tool is provided with use command
                     if len(main_menu_command.split()) == 1:
-
-                        # List tools, don't show header, loop back in main menu
                         self.list_tools()
-                        show_header = False
-
                     elif len(main_menu_command.split()) == 2:
-                        # Grab the command, either the number or word
                         tool_choice = main_menu_command.split()[1]
-
-                        # If we're choosing the payload by numbers
-                        if tool_choice.isdigit() and\
-                                0 < int(tool_choice) <= len(self.imported_tools):
+                        if tool_choice.isdigit() and 0 < int(tool_choice) <= len(self.imported_tools):
                             tool_number = 1
-                            for key, tool_object in sorted(self.imported_tools.items()):
-                                # if the entered number matches the payload, use that payload
+                            for tool_object in sorted(self.imported_tools.values(), key=lambda x: x.cli_name):
                                 if int(tool_choice) == tool_number:
                                     tool_object.tool_main_menu()
                                 tool_number += 1
                             show_header = True
-
-                        # Else if selecting payload by name
                         else:
-                            for key, tool_object in sorted(self.imported_tools.items()):
-                                # if the entered number matches the payload, use that payload
+                            for tool_object in sorted(self.imported_tools.values(), key=lambda x: x.cli_name):
                                 if tool_choice.lower() == tool_object.cli_name.lower():
                                     tool_object.tool_main_menu()
                                     show_header = True
-
-                        # Once done with tool, clear main menu command
-                        show_header = True
-
                 elif main_menu_command.startswith('list'):
-                    # List tools, don't show header, loop back in main menu
                     self.list_tools()
-
                 elif main_menu_command.startswith('info'):
                     if len(main_menu_command.split()) == 1:
                         show_header = True
-
                     elif len(main_menu_command.split()) == 2:
-                        # Grab the command, either the number or word
                         info_choice = main_menu_command.split()[1]
-
-                        # If we're choosing the payload by numbers
-                        if info_choice.isdigit() and\
-                                0 < int(info_choice) <= len(self.imported_tools):
-                            tool_number = 1
-                            for key, tool_object in sorted(self.imported_tools.items()):
-                                # If the entered number matches the tool, use that tool
-                                if int(info_choice) == tool_number:
+                        if info_choice.isdigit() and 0 < int(info_choice) <= len(self.imported_tools):
+                            for index, tool_object in enumerate(sorted(self.imported_tools.values(), key=lambda x: x.cli_name), start=1):
+                                if int(info_choice) == index:
                                     print()
                                     print(helpers.color(tool_object.cli_name) + " => " + tool_object.description)
                                     print()
-                                tool_number += 1
-
-                        # If the entered name matches the tool, use that tool
+                                    break
                         else:
-                            for key, tool_object in sorted(self.imported_tools.items()):
+                            for tool_object in sorted(self.imported_tools.values(), key=lambda x: x.cli_name):
                                 if main_menu_command.split()[1].lower() == tool_object.cli_name.lower():
                                     print()
                                     print(helpers.color(tool_object.cli_name) + " => " + tool_object.description)
                                     print()
+                                    break
                     else:
                         show_header = True
-
                 elif main_menu_command.startswith('option'):
                     self.options_veil()
-
-                # Hidden menu option
                 elif main_menu_command.startswith('config'):
                     self.config_veil()
-
-                # Hidden menu option
                 elif main_menu_command.startswith('setup'):
                     self.setup_veil()
-
                 elif main_menu_command.startswith('update'):
                     self.update_veil()
-
                 elif main_menu_command.startswith('exit') or main_menu_command.startswith('quit'):
                     sys.exit()
 
@@ -194,46 +143,44 @@ class Conductor:
             print("\n\n" + helpers.color("^C.   Quitting...", warning=True))
             sys.exit()
 
-    # Show options
     def options_veil(self):
-        print( " [i] Veil configuration file: /etc/veil/settings.py" )
-        for i in dir(settings):
-            if i.startswith('_'): continue
-            print( " [i] {0}: {1}".format( i , eval( 'settings.' + str(i) )))
-        input( '\n\nOptions shown. Press enter to continue' )
-        return
+        print(" [i] Veil configuration file: /etc/veil/settings.py")
+        for attr_name in dir(settings):
+            if not attr_name.startswith('_'):
+                print(" [i] {}: {}".format(attr_name, getattr(settings, attr_name)))
+        input('\n\nOptions shown. Press enter to continue')
 
-    # Self update framework
     def update_veil(self):
         if settings.OPERATING_SYSTEM == "Kali":
-            os.system('apt-get update; apt-get -y install veil')
+            self.run_command(['apt-get', 'update'])
+            self.run_command(['apt-get', '-y', 'install', 'veil'])
         else:
-            os.system('git pull')
+            self.run_command(['git', 'pull'])
         input('\n\nVeil has checked for updates, press enter to continue')
-        return
 
-    # Re-run setup
     def setup_veil(self):
-        if settings.OPERATING_SYSTEM == "Kali":
-            if os.path.exists("/usr/share/veil/config/setup.sh"):
-                os.system('/usr/share/veil/config/setup.sh -f -s')
-            else:
-                print("\n [!] ERROR: Kali is missing %s\n" % ("/usr/share/veil/config/setup.sh"))
-                os.system('./config/setup.sh -f -s')
-        else:
-            os.system('./config/setup.sh -f -s')
-        input('\n\nVeil has ran setup.sh, press enter to continue')
-        return
+        setup_script_path = "/usr/share/veil/config/setup.sh" if settings.OPERATING_SYSTEM == "Kali" else "./config/setup.sh"
+        self.run_script(setup_script_path, ['-f', '-s'], "Veil setup.sh")
 
-    # Re-run config.py
     def config_veil(self):
-        if settings.OPERATING_SYSTEM == "Kali":
-            if os.path.exists("/usr/share/veil/config/update-config.py"):
-                os.system('cd /usr/share/veil/config/; ./update-config.py')
-            else:
-                print("\n [!] ERROR: Kali is missing %s\n" % ("/usr/share/veil/config/update-config.py"))
-                os.system('cd ./config/; ./update-config.py')
+        config_script_path = "/usr/share/veil/config/update-config.py" if settings.OPERATING_SYSTEM == "Kali" else "./config/update-config.py"
+        self.run_script(config_script_path, [], "Veil update-config.py")
+
+    def run_command(self, command):
+        try:
+            subprocess.run(command, check=True)
+        except subprocess.CalledProcessError as e:
+            print("\n [!] ERROR: Command '{}' failed with error code {}. Exiting...\n".format(' '.join(command), e.returncode))
+            sys.exit(1)
+
+    def run_script(self, script_path, args, script_name):
+        if os.path.exists(script_path):
+            try:
+                subprocess.run([script_path] + args, check=True)
+            except subprocess.CalledProcessError as e:
+                print("\n [!] ERROR: {} script failed with error code {}. Exiting...\n".format(script_name, e.returncode))
+                sys.exit(1)
         else:
-            os.system('cd ./config/; ./update-config.py')
-        input('\n\nVeil has reconfigured, press enter to continue')
-        return
+            print("\n [!] ERROR: {} not found.\n".format(script_path))
+            sys.exit(1)
+
